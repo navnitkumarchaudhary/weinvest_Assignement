1ff297bc50aebee8ae41316e809e9ca03f751a96
navnit <navnit.chaudhary@misport.com>
2020-04-02 18:22:39 +0530

    Updates

---
+++ precache-manifest.057e6b3fbad2e53d1beeb208410a2d87.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b0aeca9f1139f8832835723879e2ae7",
+    "url": "/weinvest_Assignement/index.html"
+  },
+  {
+    "revision": "e9beec8e894d23dfc866",
+    "url": "/weinvest_Assignement/static/css/main.df3badc9.chunk.css"
+  },
+  {
+    "revision": "16a6cdefeb8159edae70",
+    "url": "/weinvest_Assignement/static/js/2.cff49b24.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/weinvest_Assignement/static/js/2.cff49b24.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9beec8e894d23dfc866",
+    "url": "/weinvest_Assignement/static/js/main.9879dbc6.chunk.js"
+  },
+  {
+    "revision": "6b28bbd32ccc13226617",
+    "url": "/weinvest_Assignement/static/js/runtime-main.8d9a5556.js"
+  },
+  {
+    "revision": "4cffa739f751f90686ec3ac6e9b56e0d",
+    "url": "/weinvest_Assignement/static/media/icon-check.4cffa739.svg"
+  }
+]);
